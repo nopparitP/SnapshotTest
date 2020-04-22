b9cf8af80e877b7d7b74581729f83346a427424c
fastwork366 <fastwork366@gmail.com>
2020-04-22 13:27:07 +0700

    Initial Commit

---
+++ SnapshotTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnapshotTest
+//
+//  Created by devKai on 22/4/2563 BE.
+//  Copyright © 2563 snapshot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
